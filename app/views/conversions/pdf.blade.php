39ad998d97dcbb5fd19757b989ebcb3bb12a64a1
Petros Diveris <petros@diveris.org>
2014-10-30 11:09:20 +0000

    Set the PDF viewer's height to the browser's viewport height

--- app/views/conversions/pdf.blade.php
+++ app/views/conversions/pdf.blade.php
@@ -6,4 +6,9 @@
  * Time: 15:59
  */
 ?>
-<iframe id="viewer" src = "/3rdparty/ViewerJS/index.html/#../../resourcecache/{{$file}}" width="100%" height="760" allowfullscreen webkitallowfullscreen></iframe>
+<iframe id="viewer" class="autoHeight" src = "/3rdparty/ViewerJS/index.html/#../../resourcecache/{{$file}}" width="100%" height="200" allowfullscreen webkitallowfullscreen></iframe>
+<script>
+  $('#viewer').load(function () {
+    $(this).height($(window).height());
+  });
+</script>
